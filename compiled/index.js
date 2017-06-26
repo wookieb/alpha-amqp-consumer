bba78c5bb6565b784d32ab396ffc3bd112284244
Łukasz Kużyński <lukasz.kuzynski@businesshub.com>
2017-06-26 13:19:30 +0200

    * Export ConsumerManager and RetryTopology * Version 0.4.1

--- compiled/index.js
+++ compiled/index.js
@@ -4,8 +4,10 @@ var Message_1 = require("./Message");
 exports.Message = Message_1.default;
 var Consumer_1 = require("./Consumer");
 exports.Consumer = Consumer_1.default;
-const ConsumerManager_1 = require("./ConsumerManager");
+var ConsumerManager_1 = require("./ConsumerManager");
+exports.ConsumerManager = ConsumerManager_1.default;
+const ConsumerManager_2 = require("./ConsumerManager");
 function connect(url, options) {
-    return ConsumerManager_1.default.connect(url, options);
+    return ConsumerManager_2.default.connect(url, options);
 }
 exports.connect = connect;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export {default as Message} from "./Message";
 export {default as Consumer, ConsumerOptions, ConsumerFunction, ACKType, RejectType} from "./Consumer";
+export {default as ConsumerManager, RetryTopology} from './ConsumerManager';
 import {ConnectionManagerOptions} from 'alpha-amqp-connection-manager';
 import ConsumerManager from './ConsumerManager';
 
